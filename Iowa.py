b0f0bbc6a1b8b3e28cb9273a58cd35c74e7f119a
Catherine <48567629+Slesrchik@users.noreply.github.com>
2020-05-13 16:06:48 +0300

    Update Iowa.py

--- Iowa.py
+++ Iowa.py
@@ -4,33 +4,37 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 import time
 # import data
+import xlrd
 import pandas as pd
 import os
 import datetime
 time_of_parsing=datetime.datetime.today().strftime('%Y-%m-%d')
-County_cases=[]
-Death_by_county=[]
-State_cases=[]
-Death_by_state=[]
 County=[]
+Cases=[]
+Deaths=[]
+
 options = Options()
-options.add_argument('--headless')
+# options.add_argument('--headless')
 # options.add_argument("download.default_directory=/Users/giova/Documents/Data Mining/COVID-19")#change path what you want
+
 driver = selenium.webdriver.Chrome('D:\chromedriver', options=options)
-driver.get('https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/county-map.html?state=IA')
-time.sleep(5)
-# table=driver.find_element_by_class_name('allborders rowbackgrounds')
-tbody=driver.find_element_by_tag_name('tbody')
-tr=tbody.find_elements_by_tag_name('tr')
-for i in tr:
-    County.append(i.find_elements_by_tag_name('td')[0].text)
-    County_cases.append((i.find_elements_by_tag_name('td')[1].text))
-    Death_by_county.append((i.find_elements_by_tag_name('td')[3].text))
+
+driver.get('https://coronavirus.iowa.gov/pages/access')
+time.sleep(20)
+tbody=driver.find_elements_by_tag_name('tbody')[0]
+
+table=tbody.find_elements_by_tag_name('tr')
+for data in table:
+ County.append(data.find_elements_by_tag_name('td')[0].text)
+ Cases.append(data.find_elements_by_tag_name('td')[1].text)
+ Deaths.append(data.find_elements_by_tag_name('td')[2].text)
+
 data= pd.DataFrame({
-    'State':'Iowa',
-    'County':County,
-    'Cases': County_cases,
-    'Deaths':Death_by_county,
-    'Date_parsing': time_of_parsing
+'State':'Iowa',
+'County':County,
+'Cases': Cases,
+'Deaths':Deaths,
+'Date_parsing': time_of_parsing
 })
-data.to_csv('D:\Iowa.csv')
+
+data.to_csv('Iowa.csv')
