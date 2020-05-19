9ede531524d8a2e83a3a82e8e0e47de1d98eaf22
Catherine <48567629+Slesrchik@users.noreply.github.com>
2020-05-19 18:25:08 +0300

    Update Indiana.py

--- Indiana.py
+++ Indiana.py
@@ -1,36 +1,11 @@
-import selenium
-from selenium import webdriver
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
-import time
-# import data
-import pandas as pd
-import os
+import requests
 import datetime
-time_of_parsing=datetime.datetime.today().strftime('%Y-%m-%d')
-County_cases=[]
-Death_by_county=[]
-State_cases=[]
-Death_by_state=[]
-County=[]
-options = Options()
-options.add_argument('--headless')
-# options.add_argument("download.default_directory=/Users/giova/Documents/Data Mining/COVID-19")#change path what you want
-driver = selenium.webdriver.Chrome('D:\chromedriver', options=options)
-driver.get('https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/county-map.html?state=IN')
-time.sleep(5)
-# table=driver.find_element_by_class_name('allborders rowbackgrounds')
-tbody=driver.find_element_by_tag_name('tbody')
-tr=tbody.find_elements_by_tag_name('tr')
-for i in tr:
-    County.append(i.find_elements_by_tag_name('td')[0].text)
-    County_cases.append((i.find_elements_by_tag_name('td')[1].text))
-    Death_by_county.append((i.find_elements_by_tag_name('td')[3].text))
-data= pd.DataFrame({
-    'State':'Indiana',
-    'County':County,
-    'Cases': County_cases,
-    'Deaths':Death_by_county,
-    'Date_parsing': time_of_parsing
-})
-data.to_csv('D:\Indiana.csv')
+import pandas as pd
+import csv
+import codecs
+req = requests.get('https://hub.mph.in.gov/dataset/89cfa2e3-3319-4d31-a60d-710f76856588/resource/8b8e6cd7-ede2-4c41-a9bd-4266df783145/download/covid_report_county.xlsx')
+url_content = req.content
+csv_file = open('D:\Indiana.csv', 'wb')
+
+csv_file.write(url_content)
+csv_file.close()
