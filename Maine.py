070fd2481f5e11f911717aea3668cd057f0a2071
Catherine <48567629+Slesrchik@users.noreply.github.com>
2020-05-20 12:59:04 +0300

    Update Maine.py

--- Maine.py
+++ Maine.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import datetime
+date_parsing = datetime.datetime.today().strftime('%Y-%m-%d')
 d = pd.read_html ("https://www.maine.gov/dhhs/mecdc/infectious-disease/epi/airborne/coronavirus.shtml")
 df = d[2]
-df.to_csv('Maine.csv')
+df.insert(1,'State','Maine')
+df.insert(6,'Date',date_parsing)
+
+df.to_csv('D:\Maine.csv')
